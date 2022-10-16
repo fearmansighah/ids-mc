6df1fc308926fe13465a56893ead511b0c3d4fdc
fearmansighah <e.firmansyah@outlook.com>
2022-10-16 00:46:12 -0700

    first commit

---
+++ mqtt-demo/subscriber/sub.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+import time
+import psycopg2
+
+# create classes
+
+
+class receiver:
+
+    def __init__(self, name, client, broker):
+        self.name = name
+        self.client = client
+        self.broker = broker
+
+    def on_message(self, client, userdata, message):
+        msg = str(message.payload.decode("utf-8"))
+        
+        print("Received message: ", msg)
+
+    def listen(self):
+        self.client.connect(self.broker)
+        self.client.loop_start()
+        self.client.subscribe("esp32-ids-ubco/temperature")
+        self.client.on_message = self.on_message
+        time.sleep(5000)
+        self.client.loop_end()
+
+
+def main():
+    mqttBroker = "test.mosquitto.org"
+    
+    server = receiver(name="server",
+                      client=mqtt.Client("Server"),
+                      broker=mqttBroker)
+    server.listen()
+
+
+if __name__ == "__main__":
+    main()
